1b3c66256459220293e0ff61ad66724b86939847
gonnagetapower <karpusha201@mail.ru>
2022-11-21 22:22:25 +0300

    add__header-chatRoom

--- studentx/src/components/Person/Person.jsx
+++ studentx/src/components/Person/Person.jsx
@@ -3,14 +3,26 @@ import nullPhoto from './../../img/nullPhotoIcon.svg';
 
 import './Person.css';
 
-const Person = ({ name, descr, status }) => {
+const Person = ({ name, descr, status, colorText }) => {
   return (
     <div className="person">
       <img className="person__icon" src={nullPhoto} alt="" />
       <div className="person-info">
-        <h1 className="person-info__name">{name}</h1>
-        <p className="person-info__descr">{descr}</p>
-        <p className="person-info__status">{status}</p>
+        <h1
+          style={colorText === 'white' ? { color: '#fff' } : { color: '#232036' }}
+          className="person-info__name">
+          {name}
+        </h1>
+        <p
+          style={colorText === 'white' ? { color: '#fff' } : { color: '#232036' }}
+          className="person-info__descr">
+          {descr}
+        </p>
+        <p
+          style={colorText === 'white' ? { color: '#fff' } : { color: '#232036' }}
+          className="person-info__status">
+          {status}
+        </p>
       </div>
     </div>
   );
