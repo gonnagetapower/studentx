5900811965a0d76dcb7aa666c8ae71bdb7bb0e61
gonnagetapower <karpusha201@mail.ru>
2022-11-22 22:19:19 +0300

    change__CreateTaskUI

--- studentx/src/views/CreateTask/CreateTask.jsx
+++ studentx/src/views/CreateTask/CreateTask.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
 import { useRouter } from '@happysanta/router';
+import { PAGE_HOME, router, MODAL_DISCIPLINE, MODAL_TOWNS, MODAL_INSTITUTE } from '../../router';
 
 import { Icon28ChevronBack } from '@vkontakte/icons';
 import { PanelHeader, PanelHeaderBack, PanelHeaderButton, Panel } from '@vkontakte/vkui';
 
 import './CreateTask.css';
 
-import { Navigation, FilterItem } from '../../components';
+import { Navigation, FilterItem, InputItem } from '../../components';
+import { DateInput } from '@vkontakte/vkui';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setPrice } from '../../redux/slices/filterSlice';
+import { setDiscipline } from '../../redux/slices/filterSlice';
 
 const CreateTask = ({ id }) => {
   const router = useRouter();
   const [photoList, setPhotoList] = useState([1]);
+  const [dateFrom, setDateFrom] = useState();
+  const [dateTo, setDateTo] = useState();
+
+  const filterState = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
 
   const handleClick = () => {
     const id = photoList.length + 1;
@@ -55,12 +67,47 @@ const CreateTask = ({ id }) => {
           />
           <div className="photoList">
             {photoList.map((photo, key) => (
-              <input key={key} id={photo.id} className="custom-file-input" type="file" />
+              <input
+                key={key}
+                id={photo.id}
+                className="custom-file-input"
+                type="file"
+                accept=".png, .jpg, .jpeg"
+              />
             ))}
             <div onClick={() => handleClick()} className="addPhoto"></div>
           </div>
           <div className="create__filter">
-            <FilterItem subTitle={'Выбрать фильтры'} />
+            <h2 className="filter-modal__title">Предмет</h2>
+            <FilterItem
+              subTitle={filterState.discipline}
+              setDiscipline={setDiscipline}
+              subModal={MODAL_DISCIPLINE}
+            />
+            <h2 className="filter-modal__title">Город</h2>
+            <FilterItem
+              subTitle={filterState.town}
+              setDiscipline={setDiscipline}
+              subModal={MODAL_TOWNS}
+            />
+            <h2 className="filter-modal__title">Учебное заведение</h2>
+            <FilterItem
+              subTitle={filterState.institute}
+              setDiscipline={setDiscipline}
+              subModal={MODAL_INSTITUTE}
+            />
+            <h2 className="filter-modal__title">Сроки</h2>
+            <div className="filter-modal__datepicker">
+              <DateInput value={dateFrom} onChange={setDateFrom} />
+              <DateInput value={dateTo} onChange={setDateTo} />
+            </div>
+            <h2 className="filter-modal__title">Желаемый бюджет</h2>
+            <InputItem
+              price={filterState.price}
+              dispatch={dispatch}
+              setPrice={setPrice}
+              title={'Цена, ₽ '}
+            />
           </div>
           <div className="create__button">
             <button className="button">Опубликовать</button>
