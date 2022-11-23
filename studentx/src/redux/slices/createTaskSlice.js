8951fc904981a3f2093404061c5cfc92bccb37b3
gonnagetapower <karpusha201@mail.ru>
2022-11-23 21:25:59 +0300

    fix__respondPage

---
+++ studentx/src/redux/slices/createTaskSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const createTaskSlice = createSlice({
+    name: 'createTask',
+    initialState: {
+        discipline: "",
+        town: "",
+        institute: "",
+        dateFrom: new Date(),
+        dateTo: new Date(),
+        price: "",
+    },
+    reducers: {
+        setDiscipline: (state, action) => {
+            state.discipline = action.payload
+        },
+        setTown: (state, action) => {
+            state.town = action.payload
+        },
+        setInstitute: (state, action) => {
+            state.institute = action.payload
+        },
+        setDateFrom: (state, action) => {
+            state.dateFrom = action.payload
+        },
+        setDateTo: (state, action) => {
+            state.dateTo = action.payload
+        },
+        setPrice: (state, action) => {
+            state.price = action.payload
+        }
+    },
+})
+
+export const { setDiscipline, setTown, setInstitute, setDateFrom, setDateTo, setPrice } = createTaskSlice.actions;
+
+export default createTaskSlice.reducer
