127c10f9a56af7f6a479b43162c61b772323b257
gonnagetapower <karpusha201@mail.ru>
2022-11-27 13:56:36 +0300

    add__PublicationPage

--- studentx/src/views/MyPublication/MyPublication.jsx
+++ studentx/src/views/MyPublication/MyPublication.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 
 import { Panel, Group } from '@vkontakte/vkui';
-import { Navigation } from '../../components';
+import { Navigation, Person, Task } from '../../components';
+import { useRouter } from '@happysanta/router';
+
+import dots from './../../img/3dots.svg';
+
+import './MyPublication.css';
 
 const MyPublication = ({ id }) => {
+  const router = useRouter();
   return (
     <Panel id={id}>
-      <Group style={{ height: '1000px' }}>
-        <h1>Publications Page</h1>
-      </Group>
+      <div className="publication__header">
+        <div onClick={() => router.popPage()} className="arrow-left"></div>
+        <h1 className="publication__title">Мои публикации</h1>
+        <img className="publication__dots" src={dots} alt="" />
+      </div>
+      <div className="publication__container">
+        <Task
+          title={' Решить метрологию '}
+          descr={
+            'Нужно решить метрологию блин ничего не понимаю вообще помогите пожалуйста иначе я выпилюсь нахрен оч тяжко жить '
+          }
+          dateOrder={'23 сентября, 09:32'}
+          price={'1.000 '}
+        />
+        <Task
+          title={' Решить метрологию '}
+          descr={
+            'Нужно решить метрологию блин ничего не понимаю вообще помогите пожалуйста иначе я выпилюсь нахрен оч тяжко жить '
+          }
+          dateOrder={'23 сентября, 09:32'}
+          price={'1.000 '}
+        />
+        <Task
+          title={' Решить метрологию '}
+          descr={
+            'Нужно решить метрологию блин ничего не понимаю вообще помогите пожалуйста иначе я выпилюсь нахрен оч тяжко жить '
+          }
+          dateOrder={'23 сентября, 09:32'}
+          price={'1.000 '}
+        />
+      </div>
       <Navigation />
     </Panel>
   );
