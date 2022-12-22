b6c5bf86b28cfb3da21bb8a753a5dd0bcfcba8e3
gonnagetapower <karpusha201@mail.ru>
2022-12-22 15:27:59 +0300

    fixValidate

--- studentx/src/components/FormInput/FormInput.jsx
+++ studentx/src/components/FormInput/FormInput.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
-const FormInput = () => {
+const FormInput = ({ title, descr, handleChange }) => {
   return (
-    <div>
-      <h1>FormInput</h1>
-    </div>
+    <>
+      {formErrors.title && <span className="errorMsg">{formErrors.title}</span>}
+      <textarea
+        type="text"
+        name="descr"
+        placeholder="Опиши задачу, чем больше подробностей, тем лучше результат :)"
+        value={formValues.descr}
+        onChange={handleChange}
+        onFocus={(e) => setFormErrors({ ...formErrors, [e.target.name]: false })}
+        onBlur={
+          formValues.title === ''
+            ? (e) => setFormErrors({ ...formErrors, [e.target.name]: true })
+            : ''
+        }
+        className={
+          formErrors.descr
+            ? 'create__input create__input--descr error'
+            : 'create__input create__input--descr'
+        }
+      />
+    </>
   );
 };
 
