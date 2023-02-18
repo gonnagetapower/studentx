04c115941c7e9784192924a006c382afdcc7d432
gonnagetapower <karpusha201@mail.ru>
2023-02-18 21:06:32 +0300

    401error

--- studentx/src/http/auth.js
+++ studentx/src/http/auth.js
@@ -9,4 +9,21 @@ $auth.interceptors.request.use((config) => {
     return config;
 })
 
+$auth.interceptors.response.use((config) => {
+    return config;
+},async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get(`https://mtimofeev.fun/auth/jwt/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.access);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('НЕ АВТОРИЗОВАН')
+        }
+    }
+    throw error;
+})
+
 export default $auth;
