c87d64acc557579b58b4ba144eef718efc1f9396
gonnagetapower <karpusha201@mail.ru>
2023-02-18 19:51:45 +0300

    addToken

--- studentx/src/http/api.js
+++ studentx/src/http/api.js
@@ -10,7 +10,7 @@ const $authHost = axios.create({
 
 const authInterceptor = config => {
     // config.headers.authorization = `Bearer ${localStorage.getItem('token')}` vk storage
-    // config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
+    config.headers.authorization = `Bearer ${localStorage.getItem('tokenAccess')}`
     return config
 }
 
