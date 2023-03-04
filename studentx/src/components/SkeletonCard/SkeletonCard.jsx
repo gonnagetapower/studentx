848db735d300e761f59095d92724f9f5d806f41f
gonnagetapower <karpusha201@mail.ru>
2023-03-04 13:36:07 +0300

    addAdaptiveTask&&Skeleton

--- studentx/src/components/SkeletonCard/SkeletonCard.jsx
+++ studentx/src/components/SkeletonCard/SkeletonCard.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
+import './SkeletonCard.css'
+
 const SkeletonCard = () => {
   return (
     <SkeletonTheme baseColor="#E7E7E7" highlightColor="#B9B9B9">
       <div className="content-item">
         <div className='task'>
-          <h1 className="content-item__title">
-            <Skeleton width={173} height={21} />
+          <h1 className="content-item__title skeleton__title">
+            <Skeleton width={1730} height={21} />
           </h1>
-          <p className="content-item__descr">
-            <Skeleton width={300} height={55} />
+          <p className="content-item__descr skeleton__descr">
+            <Skeleton width={3000} height={55} />
           </p>
           <div className="content-info">
             <p className="content-info__date">
@@ -20,9 +22,9 @@ const SkeletonCard = () => {
             <p className="content-info__price">
               <Skeleton width={40} height={20} />
             </p>
-            <p className="content-info__price">
+            {/* <p className="content-info__price">
               <Skeleton width={70} height={20} />
-            </p>
+            </p> */}
           </div>
         </div>
       </div>
