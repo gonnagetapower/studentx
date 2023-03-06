dfb2e9af1f03436c3fb1695c9b4c71e2276b2fe2
gonnagetapower <karpusha201@mail.ru>
2023-03-06 18:46:42 +0300

    addAnimation

--- studentx/src/panels/Welcome/Welcome.jsx
+++ studentx/src/panels/Welcome/Welcome.jsx
@@ -4,6 +4,8 @@ import { PAGE_HOME, PAGE_MAIN, router } from '../../router';
 import bridge from '@vkontakte/vk-bridge';
 
 import WelcomeIcon from './../../img/Welcome.svg';
+import Lottie from "lottie-react";
+import book from './../../img/animation/book.json'
 
 import './Welcome.css';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,7 +14,7 @@ import { setMyProfile } from '../../redux/slices/profileSlice';
 
 const Welcome = ({ id }) => {
 
-  const isAuth = useSelector((state) => state.app.isAuth )
+  const isAuth = useSelector((state) => state.app.isAuth)
   const dispatch = useDispatch()
 
   console.log(isAuth)
@@ -47,7 +49,10 @@ const Welcome = ({ id }) => {
     <Panel id={id}>
       <div className="welcome">
         <div className="welcome-content">
-          <img className="welcome__icon" src={WelcomeIcon} alt="Welcome" />
+          {/* <img className="welcome__icon" src={WelcomeIcon} alt="Welcome" /> */}
+          <div className="welcome__icon">
+            <Lottie animationData={book} loop={true} />
+          </div>
           <h1 className="welcome__title">Student Exchange</h1>
         </div>
       </div>
